fe1ece42f17b3d74f9ca9ca27883657346d9a553
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-06-21 23:52:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  addFeedback = (event) => {
+    this.setState((prevState) => ({...this.state, [event.target.name]:  prevState[event.target.name]+1}))}
+  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state
+   return [good.length] + [neutral.length] + [bad.length]
+   
+  }
+  
+  countPositiveFeedbackPercentage = () => {
+
+  }
+  render() {
+    return (
+      <div>
+        <p>Please leave feedback</p>
+        <button type="button" name="good" onClick={this.addFeedback}>
+          Good
+        </button>
+        <button type="button" name="neutral" onClick={this.addFeedback}>
+          Neutral
+        </button>
+        <button type="button" name="bad" onClick={this.addFeedback}>
+          Bad
+        </button>
+        <p>Statistics</p>
+        <span >Good:{this.state.good}</span>
+        <span>Neutral:{this.state.neutral}</span>
+        <span>Bad:{this.state.bad}</span>
+        <p>Total: {this.countTotalFeedback}</p>
+      </div>
+    );
+  };
+}
